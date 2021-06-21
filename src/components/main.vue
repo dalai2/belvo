e262e7771b77bc0d0e08b580d6287ea44e16ed3a
dalai <dalai2123@hotmail.com>
2021-06-21 16:05:46 -0500

    test commit

---
+++ src/components/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="result" class="content">
+    <p>User ID: {{result.userId}}</p>
+    <p>Title: {{result.title}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name:"Main",
+  created() {
+    axios.get("https://jsonplaceholder.typicode.com/todos/1").then((result) => {
+      console.log(result.data);
+    })
+  }
+};
+</script>
+
+<style>
+</style>
